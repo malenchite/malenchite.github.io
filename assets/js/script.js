5561cc417b1fe4708a538072d4b2888dc0649720
Steven Israel <malenchite@gmail.com>
2021-02-12 11:59:56 -0500

    Added correct behavior to Contact form

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+/* Submit button behavior for contact form */
+$("#contact-submit").click((event) => {
+
+  event.preventDefault();
+  let formData = {
+    name: $("#name-input").val(),
+    email: $("#email-input").val(),
+    message: $("#message-input").val()
+  };
+
+  $("#contact-status").text("Sending...");
+
+  $.ajax("http://malenchite-contact.herokuapp.com/contact",
+    {
+      type: "POST",
+      data: JSON.stringify(formData),
+      dataType: "json",
+      contentType: "application/json"
+    })
+    .then(() => {
+      $("#contact-status").text("Message submitted!");
+      $("#name-input").val("");
+      $("#email-input").val("");
+      $("#message-input").val("");
+    })
+    .catch(() => {
+      $("#contact-status").text("Error");
+    });
+});
