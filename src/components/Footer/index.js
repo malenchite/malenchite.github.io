6051d50dfd0f32242820204dad943b7e1fcb0d22
Steven Israel <malenchite@gmail.com>
2021-04-19 21:26:20 -0400

    Added sr text and animation to footer GitHub icon

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,10 +4,14 @@ function Footer() {
   return (
     <footer className="footer mt-auto py-3 fixed-bottom">
       <div className="container text-center">
-        <span className="text-muted">Copyright &copy;2020-2021 Steven Israel </span><a
-          href="https://github.com/malenchite/malenchite.github.io" target="_blank" rel="noreferrer"><i className="fab fa-github gh-link"></i></a>
+        <span className="text-muted">Copyright &copy;2020-2021 Steven Israel </span>
+        <button className="gh-button">
+          <a href="https://github.com/malenchite/malenchite.github.io" target="_blank" rel="noreferrer">
+            <i className="fab fa-github gh-icon"></i><span className="sr-only"> GitHub Repo</span>
+          </a>
+        </button>
       </div>
-    </footer>
+    </footer >
   );
 }
 
